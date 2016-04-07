d10e53039cf86c8bec1eb217172e2974edf816e9
Samuel Ramos Barroso <alu0100815146@ull.edu.es>
2016-04-07 14:33:15 +0000

    Testeando fallos con Travis

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,11 @@ main = function() {
   var template = fillTable.innerHTML;
   finaltable.innerHTML = _.template(template, {items: r});
 };
- /*
+ 
 $(document).ready(function () {
   // If the browser supports localStorage and we have some stored data
   if (window.localStorage && localStorage.original) {
     original.value = localStorage.original;
   }
   $("button").click(main);
-});*/ 
+});
